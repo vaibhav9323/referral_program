7de47c429bbb324f41d05b71c3a1b87957df041a
vaibhav singala <singalavaibhav@gmail.com>
2025-04-14 15:15:38 +0530

    refactored urls file

--- app/urls.py
+++ app/urls.py
@@ -2,8 +2,7 @@
 from django.urls import path
 
 # Make sure to import the new API view classes
-from .views import (UserLoginAPIView, UserReferralsAPIView,
-                    UserRegistrationAPIView)
+from .views import UserLoginAPIView, UserReferralsAPIView, UserRegistrationAPIView
 
 urlpatterns = [
     # Use UserRegistrationAPIView.as_view() instead of register_view
